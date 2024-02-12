cd38517c37322bd0bc90ee5dffbfa995ff9780ba
Eugene <117640691+zelenolis@users.noreply.github.com>
2024-02-12 17:39:28 +0000

    feat: mock done

--- src/05-partial-mocking/index.test.ts
+++ src/05-partial-mocking/index.test.ts
@@ -1,8 +1,15 @@
 // Uncomment the code below and write your tests
-// import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
+import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
 
 jest.mock('./index', () => {
-  // const originalModule = jest.requireActual<typeof import('./index')>('./index');
+  const originalModule = jest.requireActual<typeof import('./index')>('./index');
+  const mock = {
+    mockOne: jest.fn(() => true),
+    mockTwo: jest.fn(() => true),
+    mockThree: jest.fn(() => true),
+    unmockedFunction: originalModule.unmockedFunction,
+  }
+  return mock;
 });
 
 describe('partial mocking', () => {
@@ -11,10 +18,16 @@ describe('partial mocking', () => {
   });
 
   test('mockOne, mockTwo, mockThree should not log into console', () => {
-    // Write your test here
+    const consol = jest.spyOn(console, 'log');
+    mockOne();
+    mockTwo();
+    mockThree();
+    expect(consol).not.toBeCalled();
   });
 
   test('unmockedFunction should log into console', () => {
-    // Write your test here
+    const consol = jest.spyOn(console, 'log');
+    unmockedFunction();
+    expect(consol).toBeCalled();
   });
 });
