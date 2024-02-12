8dabc2fcb48332c6b2649a027059331b5628e8d8
Eugene <117640691+zelenolis@users.noreply.github.com>
2024-02-12 17:58:49 +0000

    feat: snapshot done

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,28 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const start = [1, 2, 3];
+    const finish = {next: {
+      next: {
+        next: {
+          next: null, value: null
+        }, value: 3
+      }, value: 2
+    }, value: 1};
+    
+    const actualLinkedList = generateLinkedList(start);
+    expect(actualLinkedList).toStrictEqual(finish);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const secondstart = [4, 5, 6]; // Example input values
+    const secondfinish = generateLinkedList(secondstart);
+
+    // Use Jest's snapshot matcher
+    expect(secondfinish).toMatchSnapshot();
   });
 });
